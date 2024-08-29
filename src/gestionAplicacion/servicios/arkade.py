6a3c07d6eeae60229f3ac7c2da45196df37ae9e0
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-08-28 23:26:59 -0500

    Version 0.4.1
    Se crean todos los modulos y paquetes, se añade la logica de las clases: Bono, Arkade y TarjetaCinemar

---
+++ src/gestionAplicacion/servicios/arkade.py
@@ -0,0 +1,122 @@
+class Arkade:
+
+  PUNTUACION_MAXIMA = 10.0 #Atributo de clase Constante
+  
+  #Inicializador
+  def __init__(self,nombreServicio= None,valorServicio = 0 ,generoServicio= None):
+
+    self._nombreServicio = nombreServicio
+    self._valorServicio = valorServicio         #Atributos y encapsulamiento
+    self._generoServicio = generoServicio
+    SucursalCine.getJuegos().append(self)
+
+  @classmethod
+  def verificarTarjetasEnInventario(cls, sucursalCine):
+
+    """
+    Description: Este metodo se encarga de verificar si almenos hay 
+    alguna tarjeta disponible en el array de tarjetas en inventario
+    de la sucursal.
+
+    :param sucursalCine: Se pasa como parámetro la sucursal de cine asociada en ese momento 
+    :return boolean: Se retorna true o false dependiendo de si hay o no tarjetas.
+    """ 
+
+    value = False
+    if sucursalCine.getInventarioTarjetasCinemar().lenght() > 0:
+      value = True
+    return value
+
+  @classmethod
+  def asociarTarjetaCliente(cls, cliente):
+
+    """
+    Description: Este metodo toma la primera tarjeta cinemar disponible y le asocia el cliente,  se le asigna saldo 0,
+	y ademas, al Cliente se le asocia la tajeta cinemar y se elimina esa tarjeta de la lista de tarjetas en inventario
+    en la sucursal.
+
+    :param cliente: Se pasa como parametro el cliente que adquirirá la tarjeta
+    :return void: No hay retorno.
+
+    """ 
+
+    cliente.getCineActual().getInventarioTarjetasCinemar()[0].setDueno(cliente)
+    cliente.getCineActual().getInventarioTarjetasCinemar()[0].setSaldo(0)
+    cliente.setCuenta(cliente.getCineActual().getInventarioTarjetasCinemar()[0])
+    cliente.getCineActual().getInventarioTarjetasCinemar().pop(0)
+
+  @classmethod
+  def mostrarJuegos(cls):
+
+    """
+    Description: Este metodo se encarga de retornar el string usado
+    para mostrar al usuario los juegos por pantalla
+
+    :param None: No hay parametros
+    :return String: retorna el string con la descripcion de los juegos.
+    """
+
+    juegos = ""
+    i = 1
+    precios = [15000.0, 20000.0, 10000.0, 30000.0, 7500.0]
+
+    for juego in SucursalCine.getJuegos():
+      
+      if juego.getValorServicio() == precios[i - 1]:
+        juegos += f"{i}. {juego.nombreServicio}--{juego.generoServicio}--{juego.valorServicio}.\n"
+      else:
+        juegos += f"{i}. {juego.nombreServicio}--{juego.generoServicio}--{juego.valorServicio} --> Precio anterior: {precios[i - 1]}.\n"
+      i += 1
+
+    juegos += "6. Volver al inicio\n7. Salir\n"
+    return f"¿Cuál juego desea jugar?\n{juegos}"
+
+ 
+
+  @classmethod
+  def aplicarDescuentoJuegos(cls, genero):
+
+    """
+    Description: Este metodo se encarga de aplicar un descuento del 20% 
+    al valor de los juegos que tengan igual genero al género pasado en el parámetro.
+
+    :param genero: Se pasa como parámetro el género del juego al cual aplicar el descuento.
+    :return Void: No hay retorno.
+    """ 
+    
+    for juego in SucursalCine.getJuegos():
+        if juego.getGeneroServicio() == genero:
+            juego.setValorServicio(juego.getValorServicio() - (juego.getValorServicio() * 20 / 100))
+
+  @classmethod
+  def reestablecerPrecioJuegos(cls):
+    
+    SucursalCine.getJuegos()[0].setValorServicio(15000)
+    SucursalCine.getJuegos()[1].setValorServicio(20000)
+    SucursalCine.getJuegos()[2].setValorServicio(10000)
+    SucursalCine.getJuegos()[3].setValorServicio(30000)
+    SucursalCine.getJuegos()[4].setValorServicio(7500)
+
+  # Metodos get
+  def getNombreServicio(self):
+    return self._nombreServicio
+
+  def getValorServicio(self):
+    return self._valorServicio
+
+  @classmethod
+  def getPuntuacionMaxima(cls):
+    return cls.PUNTUACION_MAXIMA
+
+  def getGeneroServicio(self):
+    return self._generoServicio
+
+  # Metodos set
+  def setNombreServicio(self, nombreServicio):
+    self._nombreServicio = nombreServicio
+
+  def setValorServicio(self, valorServicio):
+    self._valorServicio = valorServicio
+
+  def setGeneroServicio(self, generoServicio):
+    self._generoServicio = generoServicio
---
+++ src/gestionAplicacion/servicios/bono.py
@@ -0,0 +1,123 @@
+import random
+
+class Bono:
+  
+  def __init__(self, codigo = None, producto = None, tipoServicio = None, cliente = None):
+    self._codigo = codigo
+    self._producto = producto
+    self._tipoServicio = tipoServicio
+    self._cliente = cliente
+    cliente.getCineActual().getBonosCreados().append(self)
+
+  @classmethod
+  def generarBonoComidaJuegos(cls,sucursal, cliente): 
+
+    """
+    Description: Este metodo se encarga primeramente de seleccionar los productos de tipo comida
+    del inventario de la sucursal, luego genera un codigo aleatorio de 7 digitos para el bono y 
+    ademas escoge de esos productos seleccionados uno de manera aleatoria para ser asociado al bono
+    y lo descuenta de la cantidad de disponibles, finalmente imprime por pantalla el bono al usuario
+
+    :param sucursal: Se pasa como parámetro la sucursal de cine asociada en ese momento 
+    :param cliente: Se pasa como parámetro el cliente que genera el bono
+    :return Bono: Se retorna el bono creado
+    """ 
+    
+    productosComida = []
+
+    for producto in sucursal.getInventarioCine():
+      if producto.getTipoProducto() == "comida" and producto.getCantidad() > 0:
+          productosComida.append(producto)
+
+    if not productosComida:
+      return None
+
+    numeroAleatorio = random.randint(0, len(productosComida) - 1)
+    code = Bono.generarCodigoAleatorio(7)
+
+    productoSeleccionado = productosComida[numeroAleatorio]
+    productoBono = Producto(productoSeleccionado.getNombre(),productoSeleccionado.getTamaño(),1,productoSeleccionado.getPrecio(),"comida")
+
+    bono = Bono(code, productoBono, productoSeleccionado.getTipoProducto(), cliente)
+    productosComida[numeroAleatorio].setCantidad(productosComida[numeroAleatorio].getCantidad()-1)
+
+    tipoProducto = "Comida"
+    Administrador.mostrarBono(productosComida, numeroAleatorio, tipoProducto, code)
+
+    return bono
+
+  @classmethod
+  def generarBonoSouvenirJuegos(cls,sucursal, cliente): 
+
+    """
+    Description: Este metodo se encarga primeramente de seleccionar los productos de tipo souvenir
+    del inventario de la sucursal, luego genera un codigo aleatorio de 7 digitos para el bono y 
+    ademas escoge de esos productos seleccionados uno de manera aleatoria para ser asociado al bono
+    y lo descuenta de la cantidad de disponibles, finalmente imprime por pantalla el bono al usuario
+
+    :param sucursal: Se pasa como parámetro la sucursal de cine asociada en el momento 
+    :param cliente: Se pasa como parámetro el cliente que genera el bono
+    :return Bono: Se retorna el bono creado
+    """ 
+
+    productosSouvenirs = []
+
+    for producto in sucursal.getInventarioCine():
+      if producto.getTipoProducto() == "souvenir" and producto.getCantidad() > 0:
+          productosSouvenirs.append(producto)
+
+    if not productosSouvenirs:
+      return None
+
+    numeroAleatorio = random.randint(0, len(productosSouvenirs) - 1)
+    code = Bono.generarCodigoAleatorio(7)
+
+    productoSeleccionado = productosSouvenirs[numeroAleatorio]
+    productoBono = Producto(productoSeleccionado.getNombre(),productoSeleccionado.getTamaño(),1,productoSeleccionado.getPrecio(),"souvenir")
+
+    bono = Bono(code, productoBono, productoSeleccionado.getTipoProducto(), cliente)
+    productosSouvenirs[numeroAleatorio].setCantidad(productosSouvenirs[numeroAleatorio].getCantidad()-1)
+
+    tipoProducto = "Comida"
+    Administrador.mostrarBono(productosSouvenirs, numeroAleatorio, tipoProducto, code)
+
+    return bono
+
+  @classmethod
+  def generarCodigoAleatorio(cls,longitud):
+    """
+    Description: Este método se encarga de generar un código aleatorio para los bonos creados.
+  
+    :param longitud: Se pasa como parámetro la longitud que se desea para el código.
+    :return str: Se retorna el código creado.
+    """
+    caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+      
+    codigo = ''.join(random.choice(caracteres) for _ in range(longitud))
+    return codigo
+
+
+  #Getters y Setters
+  def getCodigo(self):
+    return self._codigo
+
+  def setCodigo(self, codigo):
+    self._codigo = codigo
+
+  def getTipoServicio(self):
+    return self._tipoServicio
+
+  def setTipoServicio(self, tipoServicio):
+    self._tipoServicio = tipoServicio
+
+  def getProducto(self):
+    return self._producto
+
+  def setProducto(self, producto):
+    self._producto = producto
+
+  def getCliente(self):
+    return self._cliente
+
+  def setCliente(self, cliente):
+    self._cliente = cliente
