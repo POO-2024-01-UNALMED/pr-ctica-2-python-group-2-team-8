3d8fcf016b8345d11a5e11c1b7ed3c7e1e552178
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-09-17 13:11:17 -0500

    Se pone mas bonito field frame

--- src/gestionAplicacion/usuario/metodoPago.py
+++ src/gestionAplicacion/usuario/metodoPago.py
@@ -9,7 +9,6 @@ class MetodoPago():
         self._sucursalCine = sucursalCine
 
         if (nombre != "Puntos"):
-            print(self._sucursalCine)
             self._sucursalCine.getMetodosDePagoDisponibles().append(self)
 
 
@@ -177,14 +176,14 @@ class MetodoPago():
             if (tipoMembresia == metodoPago.getTipo()):
                 totalMetodosDePagoPorTipo+=1
 
-        print(totalMetodosDePagoPorTipo)
+
 
         #En caso de que el cliente no pudo cubrir la totalidad del pago y se haya llegado al limite de ese método de pago,
 		#la acumulación de puntos solo se hara sobre el primer precio calculado luego del descuento. Los siguientes pagos ya estan cubiertos.
         if (len(clienteProceso.getMetodosDePago()) == totalMetodosDePagoPorTipo):
             if (tipoMembresia == 1):
                 puntos.setLimiteMaximoPago(puntos.getLimiteMaximoPago() + (precio * (1- self.getDescuentoAsociado())) * 0.05)
-                print(puntos.getLimiteMaximoPago())
+               
             elif (tipoMembresia == 2):
                 puntos.setLimiteMaximoPago(puntos.getLimiteMaximoPago() + (precio * (1- self.getDescuentoAsociado())) * 0.10)
             clienteProceso.setPuntos(int(puntos.getLimiteMaximoPago()))
