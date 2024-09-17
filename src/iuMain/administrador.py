8f862261c0280ac9094bc991d4b2ffb3315c3053
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-09-17 15:06:07 -0500

    creacion de __inits__

--- src/iuMain/administrador.py
+++ src/iuMain/administrador.py
@@ -6,6 +6,8 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
+
+
 import tkinter as tk
 import random
 from gestionAplicacion.servicios.bono import *
